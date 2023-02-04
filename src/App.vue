f2882f633d17edc4ec9a10810ac8da464525b98a
奚玉帆 <568875138@qq.com>
2023-02-05 01:42:15 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <HelloWorld style="min-height: 30vh;" />
-  <div style=""><br/>Copyright © 2023 <br/> 成都中莱达网络科技有限公司</div>
+  <div style=""><br/>Copyright © 2023 <br/> 成都中莱达网络科技有限公司<br/><a href="https://github.com/zldqy/font_extract_tool" target="_blank">GitHub</a></div>
 </template>
 
 <style scoped>
